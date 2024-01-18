9a0ed1e8f6b656e1ccaec415374a155f4ff2ef93
myecoria <contact@myecoria.com>
2024-01-18 22:00:38 +0100

    push

--- lib/src/account/account_type.dart
+++ lib/src/account/account_type.dart
@@ -3,18 +3,18 @@ class NanoAccountType {
   static const int NANO = 1;
   static const int BANANO = 2;
   static const String NANO_REGEX =
-      r"(xrb|nano)(_)(1|3)[13456789abcdefghijkmnopqrstuwxyz]{59}";
+      r"(xrb|nano|xro)(_)(1|3)[13456789abcdefghijkmnopqrstuwxyz]{59}";
   static const String BANANO_REGEX =
       r"(ban)(_)(1|3)[13456789abcdefghijkmnopqrstuwxyz]{59}";
 
   static String getPrefix(int type) {
     switch (type) {
       case NANO:
-        return "nano_";
+        return "xro_";
       case BANANO:
         return "ban_";
       default:
-        return "nano_";
+        return "xro_";
     }
   }
 
